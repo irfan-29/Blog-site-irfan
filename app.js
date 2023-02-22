92dd6f7048854122787fd19fd2cfc94a903fa817
Mohammed Irfan U <umirfan29@gmail.com>
2023-02-22 17:32:40 +0530

    new-4

--- app.js
+++ app.js
@@ -14,8 +14,7 @@ app.use(express.static("public"));
 mongoose.set({strictQuery: true});
 mongoose.connect("mongodb+srv://admin-irfan:Irf6360944@cluster0.jo7etur.mongodb.net/blogDB");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "Hey there! Welcome to our Blogz - a blog site. Here you can publish your blogs and visit others blogs. ";
 
 const postSchema = new mongoose.Schema({
   title: String,
@@ -43,7 +42,7 @@ app.get("/blog",function(req,res){
 });
 });
 app.get("/contact",function(req,res){
-  res.render("contact",{startingContent: contactContent});
+  res.render("contact");
 });
 app.get("/compose",function(req,res){
   res.render("compose");
@@ -63,7 +62,7 @@ app.post("/compose",function(req,res){
 });
 
 app.post("/back", function(req, res){
-  res.redirect("/");
+  res.redirect("/blog");
 });
 
 app.get("/posts/:postId",function(req,res){
