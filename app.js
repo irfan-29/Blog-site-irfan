cda1b300e098f82547681060bc30efb96571db33
Mohammed Irfan U <umirfan29@gmail.com>
2023-04-04 20:01:38 +0530

    author update

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ mongoose.connect("mongodb+srv://admin-irfan:Irf6360944@cluster0.jo7etur.mongodb.
 const homeStartingContent = "Hey there! Welcome to our Blogz - a blog site. Here you can publish your blogs and visit others blogs. ";
 
 const postSchema = new mongoose.Schema({
+  author: String,
   title: String,
   content: String
 });
@@ -54,11 +55,12 @@ app.post("/",function(req,res){
 
 app.post("/compose",function(req,res){
   const post = new Post({
+    author: req.body.postAuthor,
     title : req.body.postTitle,
     content : req.body.postContent
   });
   post.save();
-  res.redirect("/");
+  res.redirect("/blog");
 });
 
 app.post("/back", function(req, res){
@@ -69,7 +71,7 @@ app.get("/posts/:postId",function(req,res){
   const requestedPostId = req.params.postId;
   Post.findOne({_id: requestedPostId},function(err,postRender){
     if(!err){
-      res.render("post",{keyTitle: postRender.title, keyContent: postRender.content});
+      res.render("post",{keyTitle: postRender.title, keyContent: postRender.content, keyAuthor: postRender.author});
     }
   });
 });
